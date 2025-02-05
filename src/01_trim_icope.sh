9983a94397b7c518252671653d4704bae62f30d3
Søren Helweg Dam <soren.helweg@gmail.com>
2025-02-05 07:07:43 +0100

    Skip already trimmed files

--- src/01_trim_icope.sh
+++ src/01_trim_icope.sh
@@ -4,8 +4,8 @@ echo "Trimming reads"
 #alias fastp='/home/ctools/fastp/fastp'
 cd /home/projects/dp_immunoth/data/iCOPE
 mkdir -p trimmed
-# ./*
-for subfolder in ./RNAseq/11021_000001.T.T02.S2; do
+
+for subfolder in ./RNAseq/*; do
   if [ -d "$subfolder" ]; then
     # Find the forward and reverse files
     forward=$(find "$subfolder" -type f -name "*.R1.fq.gz")
@@ -17,10 +17,15 @@ for subfolder in ./RNAseq/11021_000001.T.T02.S2; do
       trimmed_forward="trimmed/$(basename "$forward")"
       trimmed_reverse="trimmed/$(basename "$reverse")"
 
-      # Run fastp
-      fastp -i "$forward" -I "$reverse" -o "$trimmed_forward" -O "$trimmed_reverse" --detect_adapter_for_pe -c --cut_front --cut_right
+      # Check if the output files already exist
+      if [ ! -f "$trimmed_forward" ] || [ ! -f "$trimmed_reverse" ]; then
+        # Run fastp
+        fastp -i "$forward" -I "$reverse" -o "$trimmed_forward" -O "$trimmed_reverse" --detect_adapter_for_pe -c --cut_front --cut_right --thread 38
+      else
+        echo "Trimmed files already exist for $subfolder"
+      fi
     else
-      echo "Warning: Missing forward or reverse file in $subfolder" >> log/out
+      echo "Warning: Missing forward or reverse file in $subfolder"
     fi
   fi
 done

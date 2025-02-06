a70a129d6dd88d4bcaaf68f8a907a6eb74ea0d6b
Søren Helweg Dam <soren.helweg@gmail.com>
2025-02-06 08:43:32 +0100

    add index to subset

--- src/03_subset_icope.sh
+++ src/03_subset_icope.sh
@@ -4,35 +4,21 @@ echo "Subsetting alignment"
 cd /home/projects/dp_immunoth/data/iCOPE
 mkdir -p cd19
 
-# Function to check if files exist
-check_files() {
-  for file in "$@"; do
-    if [ ! -f "$file" ]; then
-      return 1
-    fi
-  done
-  return 0
-}
 
-
-# ref_files=("cd19/Homo_sapiens.GRCh38.CD19.fa" "cd19/Homo_sapiens.GRCh38.107.CD19.gtf")
-# if ! check_files "${ref_files[@]}"; then
-#   echo "Subsetting reference"
-#   mkdir -p ../homo_sapiens
-#   cd ../homo_sapiens
-#   wget http://ftp.ensembl.org/pub/release-107/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
-#   wget http://ftp.ensembl.org/pub/release-107/gtf/homo_sapiens/Homo_sapiens.GRCh38.107.gtf.gz
-#   gunzip *.gz
-#   cd ../iCOPE
-# fi
-
-
-echo "Subsetting bam files"
+echo "Indexing mapped reads with samtools"
+for file in mapped/*.bam; do
+  if [ ! -f mapped/$(basename "$file").bai ]; then
+    samtools index "$file" 
+  else
+    echo "$file already indexed"
+  fi
+done
 
 for file in mapped/*.bam; do
-  if ! check_files cd19/"$file" ; then
+  if [ ! -f  cd19/"$file" ]; then
     base=$(basename "$file" .bam)
-    samtools view -b $file chr16:28931971-28939342 > cd19/$base.cd19.bam
+    
+    samtools view --write-index -b $file chr16:28931971-28939342  > cd19/$base.cd19.bam
   fi
 done
 
